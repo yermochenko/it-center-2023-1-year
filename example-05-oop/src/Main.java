8a2cb220a0ef05c232568577be9390b2b7c06392
Sergey Yermochenko <yermochenko@gmail.com>
2024-03-23 22:14:27 +0300

    Пример ООП (сборка компьютеров)

--- example-05-oop/src/Main.java
+++ example-05-oop/src/Main.java
@@ -1,35 +1,51 @@
 class Cpu {
+	// TODO: сделать методы приватными
 	String name;
 	int price;
 	int cores;
 	double speed;
 	// TODO: добавить метод out()
+	// TODO: добавить get-теры и set-теры для полей
 }
 
 class Ram {
+	// TODO: сделать методы приватными
 	int price;
 	int volume;
 	// TODO: добавить метод out()
+	// TODO: добавить get-теры и set-теры для полей
 }
 
 class SystemBlock {
-	int price;
-	int power;
-	// TODO: добавить поля "процессор" и "память"
+	private int price;
+	private int power;
+	// TODO: добавить поля "процессор" и "память" и get-теры и set-теры для них
 	public void out() {
 		System.out.println("Системный блок:");
 		System.out.println("    Цена    : " + Main.formatPrice(price));
 		System.out.println("    Мощность: " + power + " Вт");		
 		// TODO: добавить вывод информации о процессоре и памяти
 	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getPower() {
+		return power;
+	}
+	public void setPower(int power) {
+		this.power = power;
+	}
 }
 
 class Monitor {
-	String name;
-	int price;
-	double diagonal;
-	int pixelsWidth;
-	int pixelsHeight;
+	private String name;
+	private int price;
+	private double diagonal;
+	private int pixelsWidth;
+	private int pixelsHeight;
 	public void out(int number) {
 		if(number == 0) {
 			System.out.print("Монитор: ");
@@ -41,13 +57,43 @@ class Monitor {
 		System.out.println("    Диагональ : " + diagonal + "\"");
 		System.out.println("    Разрешение: " + pixelsWidth + " x " + pixelsHeight);						
 	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public double getDiagonal() {
+		return diagonal;
+	}
+	public void setDiagonal(double diagonal) {
+		this.diagonal = diagonal;
+	}
+	public int getPixelsWidth() {
+		return pixelsWidth;
+	}
+	public void setPixelsWidth(int pixelsWidth) {
+		this.pixelsWidth = pixelsWidth;
+	}
+	public int getPixelsHeight() {
+		return pixelsHeight;
+	}
+	public void setPixelsHeight(int pixelsHeight) {
+		this.pixelsHeight = pixelsHeight;
+	}
 }
 
 class Computer {
-	String name;
-	int price;
-	SystemBlock systemBlock;
-	Monitor[] monitors;
+	private String name;
+	private int price;
+	private SystemBlock systemBlock;
+	private Monitor[] monitors;
 	public void out() {
 		System.out.println("********** Компьютер **********");
 		System.out.println("Модель: " + name);
@@ -64,6 +110,30 @@ class Computer {
 		}
 		System.out.println("*******************************");
 	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public SystemBlock getSystemBlock() {
+		return systemBlock;
+	}
+	public void setSystemBlock(SystemBlock systemBlock) {
+		this.systemBlock = systemBlock;
+	}
+	public Monitor[] getMonitors() {
+		return monitors;
+	}
+	public void setMonitors(Monitor[] monitors) {
+		this.monitors = monitors;
+	}
 }
 
 public class Main {
@@ -72,46 +142,46 @@ public class Main {
 	}
 	public static void main(String[] args) {
 		Computer c1 = new Computer();
-		c1.name = "HP";
-		c1.price = 2_349__49;
-		c1.systemBlock = new SystemBlock();
-		c1.systemBlock.price = 2_000__00;
-		c1.systemBlock.power = 500;
+		c1.setName("HP");
+		c1.setPrice(2_349__95);
+		c1.setSystemBlock(new SystemBlock());
+		c1.getSystemBlock().setPrice(2_000__00);
+		c1.getSystemBlock().setPower(600);
 		// TODO: установить процессор и память
-		c1.monitors = new Monitor[] { new Monitor() };
-		c1.monitors[0].name = "LG";
-		c1.monitors[0].price = 300__00;
-		c1.monitors[0].diagonal = 19.3;
-		c1.monitors[0].pixelsWidth = 1280;
-		c1.monitors[0].pixelsHeight = 1024;
+		c1.setMonitors(new Monitor[] { new Monitor() });
+		c1.getMonitors()[0].setName("LG");
+		c1.getMonitors()[0].setPrice(490__00);
+		c1.getMonitors()[0].setDiagonal(24.3);
+		c1.getMonitors()[0].setPixelsWidth(1920);
+		c1.getMonitors()[0].setPixelsHeight(1080);
 		c1.out();
 		Computer c2 = new Computer();
-		c2.name = "Dell";
-		c2.price = 3_449__99;
-		c2.systemBlock = new SystemBlock();
-		c2.systemBlock.price = 2_400_00;
-		c2.systemBlock.power = 750;
+		c2.setName("Dell");
+		c2.setPrice(3_449__99);
+		c2.setSystemBlock(new SystemBlock());
+		c2.getSystemBlock().setPrice(2_400_00);
+		c2.getSystemBlock().setPower(750);
 		// TODO: установить процессор и память
-		c2.monitors = new Monitor[] { new Monitor(), new Monitor() };
-		c2.monitors[0].name = "Philips";
-		c2.monitors[0].price = 500__00;
-		c2.monitors[0].diagonal = 24;
-		c2.monitors[0].pixelsWidth = 1920;
-		c2.monitors[0].pixelsHeight = 1080;
-		c2.monitors[1].name = "Philips";
-		c2.monitors[1].price = 500__00;
-		c2.monitors[1].diagonal = 24;
-		c2.monitors[1].pixelsWidth = 1920;
-		c2.monitors[1].pixelsHeight = 1080;
+		c2.setMonitors(new Monitor[] { new Monitor(), new Monitor() });
+		c2.getMonitors()[0].setName("Samsung");
+		c2.getMonitors()[0].setPrice(300__00);
+		c2.getMonitors()[0].setDiagonal(21);
+		c2.getMonitors()[0].setPixelsWidth(1240);
+		c2.getMonitors()[0].setPixelsHeight(720);
+		c2.getMonitors()[1].setName("Philips");
+		c2.getMonitors()[1].setPrice(200__00);
+		c2.getMonitors()[1].setDiagonal(19);
+		c2.getMonitors()[1].setPixelsWidth(1240);
+		c2.getMonitors()[1].setPixelsHeight(1024);
 		c2.out();
 		Computer c3 = new Computer();
-		c3.name = "Apple MacMini";
-		c3.price = 4_321__89;
-		c3.systemBlock = new SystemBlock();
-		c3.systemBlock.price = 4_200_00;
-		c3.systemBlock.power = 400;
+		c3.setName("Apple MacMini");
+		c3.setPrice(4_567__89);
+		c3.setSystemBlock(new SystemBlock());
+		c3.getSystemBlock().setPrice(4_500__00);
+		c3.getSystemBlock().setPower(500);
 		// TODO: установить процессор и память
-		c3.monitors = new Monitor[0];
+		c3.setMonitors(new Monitor[0]);
 		c3.out();
 	}
 }
