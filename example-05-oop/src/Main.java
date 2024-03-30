b73921be7bd0379c962d71e18984604b66023eb1
Гость <@aud305-book-2>
2024-03-30 15:45:52 +0300

    Пример ООП (сборка компьютеров)

--- example-05-oop/src/Main.java
+++ example-05-oop/src/Main.java
@@ -1,30 +1,73 @@
 class Cpu {
-	// TODO: сделать поля приватными
-	String name;
-	int price;
-	int cores;
-	double speed;
-	// TODO: добавить метод out()
-	// TODO: добавить get-теры и set-теры для полей
+	private String name;
+	private int price;
+	private int cores;
+	private double speed;
+	public void out() {
+		System.out.println("    Процессор " + name + ":");
+		System.out.println("        Цена     : " + Main.formatPrice(price));
+		System.out.println("        К-во ядер: " + cores);
+		System.out.println("        Частота  : " + speed + " ГГц");
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getCores() {
+		return cores;
+	}
+	public void setCores(int cores) {
+		this.cores = cores;
+	}
+	public double getSpeed() {
+		return speed;
+	}
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
 }
 
 class Ram {
-	// TODO: сделать поля приватными
-	int price;
-	int volume;
-	// TODO: добавить метод out()
-	// TODO: добавить get-теры и set-теры для полей
+	private int price;
+	private int volume;
+	public void out() {
+		System.out.println("    Оперативная память:");
+		System.out.println("        Цена : " + Main.formatPrice(price));
+		System.out.println("        Объём: " + volume + " Гб");
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getVolume() {
+		return volume;
+	}
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
 }
 
 class SystemBlock {
 	private int price;
 	private int power;
-	// TODO: добавить поля "процессор" и "память" и get-теры и set-теры для них
+	private Cpu cpu;
+	private Ram ram;
 	public void out() {
 		System.out.println("Системный блок:");
 		System.out.println("    Цена    : " + Main.formatPrice(price));
-		System.out.println("    Мощность: " + power + " Вт");		
-		// TODO: добавить вывод информации о процессоре и памяти
+		System.out.println("    Мощность: " + power + " Вт");
+		cpu.out();
+		ram.out();
 	}
 	public int getPrice() {
 		return price;
@@ -37,6 +80,18 @@ class SystemBlock {
 	}
 	public void setPower(int power) {
 		this.power = power;
+	}
+	public Cpu getCpu() {
+		return cpu;
+	}
+	public void setCpu(Cpu cpu) {
+		this.cpu = cpu;
+	}
+	public Ram getRam() {
+		return ram;
+	}
+	public void setRam(Ram ram) {
+		this.ram = ram;
 	}
 }
 
@@ -147,7 +202,14 @@ public class Main {
 		c1.setSystemBlock(new SystemBlock());
 		c1.getSystemBlock().setPrice(2_000__00);
 		c1.getSystemBlock().setPower(600);
-		// TODO: установить процессор и память
+		c1.getSystemBlock().setCpu(new Cpu());
+		c1.getSystemBlock().getCpu().setName("AMD Ryzen 5");
+		c1.getSystemBlock().getCpu().setPrice(378__11);
+		c1.getSystemBlock().getCpu().setCores(6);
+		c1.getSystemBlock().getCpu().setSpeed(3.5);
+		c1.getSystemBlock().setRam(new Ram());
+		c1.getSystemBlock().getRam().setPrice(456__78);
+		c1.getSystemBlock().getRam().setVolume(16);
 		c1.setMonitors(new Monitor[] { new Monitor() });
 		c1.getMonitors()[0].setName("LG");
 		c1.getMonitors()[0].setPrice(490__00);
@@ -161,7 +223,14 @@ public class Main {
 		c2.setSystemBlock(new SystemBlock());
 		c2.getSystemBlock().setPrice(2_400_00);
 		c2.getSystemBlock().setPower(750);
-		// TODO: установить процессор и память
+		c2.getSystemBlock().setCpu(new Cpu());
+		c2.getSystemBlock().getCpu().setName("Intel Core i5");
+		c2.getSystemBlock().getCpu().setPrice(678__90);
+		c2.getSystemBlock().getCpu().setCores(8);
+		c2.getSystemBlock().getCpu().setSpeed(3.1);
+		c2.getSystemBlock().setRam(new Ram());
+		c2.getSystemBlock().getRam().setPrice(1_234__56);
+		c2.getSystemBlock().getRam().setVolume(64);
 		c2.setMonitors(new Monitor[] { new Monitor(), new Monitor() });
 		c2.getMonitors()[0].setName("Samsung");
 		c2.getMonitors()[0].setPrice(300__00);
@@ -180,7 +249,14 @@ public class Main {
 		c3.setSystemBlock(new SystemBlock());
 		c3.getSystemBlock().setPrice(4_500__00);
 		c3.getSystemBlock().setPower(500);
-		// TODO: установить процессор и память
+		c3.getSystemBlock().setCpu(new Cpu());
+		c3.getSystemBlock().getCpu().setName("Intel Core i3");
+		c3.getSystemBlock().getCpu().setPrice(543__21);
+		c3.getSystemBlock().getCpu().setCores(4);
+		c3.getSystemBlock().getCpu().setSpeed(2.7);
+		c3.getSystemBlock().setRam(new Ram());
+		c3.getSystemBlock().getRam().setPrice(987_65);
+		c3.getSystemBlock().getRam().setVolume(32);
 		c3.setMonitors(new Monitor[0]);
 		c3.out();
 	}
